5b48d092e6c1eb0a41e29d82dc3dc9c27062fc87
nakshathraa1606-crypto <nakshathraa1606@gmail.com>
2025-09-01 12:00:23 +0530

    Add files via upload

---
+++ factoriall.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	float num=14.1600;
+	printf("factorial of number %f:");
+	return 0;
+}
---
+++ print given word.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	char word[50];
+	printf("enter a word:")
+	scanf("%s",word);
+	printf("you entered:",word)
+	return 0;
+}
